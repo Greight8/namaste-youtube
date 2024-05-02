7f29b8ed0cafd3a5d8aec19a85927a8c94f3c05b
Dhruv <dhruvbhatnagar4@gmail.com>
2024-05-02 15:28:12 +0530

    Ep-3 part 2 LiveChat & inputbox-user can also chat

--- src/components/LiveChat.js
+++ src/components/LiveChat.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { useState } from 'react'
 import { useEffect } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { addMessage } from '../utils/chatSlice'
@@ -13,7 +14,9 @@ const LiveChat = () => {
         return store.chat.message
     })
 
+    const [text, setText] = useState("")
 
+    // 1) doing api polling and sending the data to store :- array of objects
     useEffect(() => {
         const timer = setInterval(() => {
             // API polling
@@ -31,6 +34,23 @@ const LiveChat = () => {
         })
     }, [])
 
+    // 2) func handleChange
+    const handleChange = (e) => {
+        console.log(text)
+        setText(e.target.value)
+    }
+
+    // 3) updating our chatSlice here :- with our own text
+    const updateChatSlice = () => {
+        dispatch(addMessage(
+            {
+                name: "akshay",
+                text: text
+            }
+        ))
+        setText("")
+    }
+
     return (
         <>
             <div className='w-full p-2 h-[450px] ml-2 border border-black bg-slate-100 rounded-lg overflow-y-scroll flex flex-col-reverse'>
@@ -40,6 +60,11 @@ const LiveChat = () => {
                     )
                 })}
             </div>
+
+            <form className='mx-3 my-1' onSubmit={(e) => { return e.preventDefault() }}>
+                <input className='border border-black w-[300px]' onChange={handleChange} value={text} type="text" />
+                <button className='mx-4 p-2 border border-black' onClick={updateChatSlice}>enter</button>
+            </form>
         </>
     )
 }
